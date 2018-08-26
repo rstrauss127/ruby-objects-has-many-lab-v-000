a7c0c872877d7a4590f2ac4938cb3fa1b1644bdb
Rebecca Strauss <rstrauss127@gmail.com>
2018-08-26 00:49:27 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    if self.author == nil
+      return nil
+    else
+      return self.author.name
+    end
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist == nil
+      return nil
+    else
+      self.artist.name
+    end
+  end
+  
+end
